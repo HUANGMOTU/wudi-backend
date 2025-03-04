7bef40c8e194196a9cf33abaf877c9558a88c3fb
HuangMoTu <2482696506@qq.com>
2025-03-04 18:39:15 +0800

    线上部署正常运行 更正了Cookie配置类的位置

--- src/main/java/com/yupi/usercenter/config/CookieConfig.java
+++ src/main/java/com/yupi/usercenter/config/CookieConfig.java
@@ -1,4 +1,4 @@
-package com.yupi.usercenter;
+package com.yupi.usercenter.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
